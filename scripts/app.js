6dd8f0b26f58c878bf3a829a9c430f8d77d05c21
Its Ya Boi Del Rey <delreeyyy@gmail.com>
2022-06-19 23:33:05 +0800

    Completed all modules for tic tac toe game

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,12 @@ const formElement = document.querySelector('form');
 const errorMessage = document.getElementById('error-message');
 const activePlayerNameElement = document.getElementById("active-player-name");
 
+const gameField = [
+    [0, 0, 0,],
+    [0, 0, 0,],
+    [0, 0, 0,]
+];
+
 const player = [
     {
         name: '',
@@ -17,6 +23,8 @@ const player = [
 
 let activePlayer = 0;
 let editedPlayer = 0;
+let currentRound = 1;
+let winnerFound = false;
 
 
 const inputPlayerName = document.getElementById('player-name');
@@ -25,6 +33,7 @@ const btnEditPlayerTwoElement = document.getElementById('btn-edit-playertwo');
 const btnCancelConfigElement = document.getElementById('btn-cancel-config');
 const btnStartNewGameElement = document.getElementById('btn-startnewgame');
 const gameFieldsElements = document.querySelectorAll("#game-board li");
+const endGameMessage = document.querySelector("#active-game article");
 
 btnEditPlayerOneElement.addEventListener('click', openPlayerConfig);
 btnEditPlayerTwoElement.addEventListener('click', openPlayerConfig);
--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,29 @@
+function resetGame(){
+    activePlayer = 0;
+    editedPlayer = 0;
+    currentRound = 1;
+    winnerFound = false;
+    let indexFields = 0;
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            gameField[i][j] = 0;
+            gameFieldsElements[indexFields].classList.remove("disabled");
+            gameFieldsElements[indexFields].textContent = '';
+            indexFields++;
+        }
+    }
+
+    endGameMessage.style.display = "none";
+    endGameMessage.firstElementChild.innerHTML = "You won, <span>PLAYER NAME</span>!"; 
+    
+}
 function startNewGame(){
     if(player[0].name === "" || player[1].name === ""){
         alert("Please input both names!");
         return;
     }
+
+    resetGame();
 
     activeGame = document.getElementById("active-game");
     activeGame.style.display = "block";
@@ -23,8 +44,87 @@ function switchPlayer(){
 }
 
 function selectedGameField(event){
-    event.target.textContent = player[activePlayer].symbol;
-    event.target.classList.add("disabled");
+    if(winnerFound){
+        return;
+    }
 
+    const selectedElement = event.target;
+    const selectedRow = selectedElement.dataset.row - 1;
+    const selectedCol = selectedElement.dataset.col - 1;
+
+    if(gameField[selectedRow][selectedCol] > 0){
+        alert("Select and empty field");
+        return; //exit function
+    }
+
+    selectedElement.textContent = player[activePlayer].symbol;
+    selectedElement.classList.add("disabled");
+    gameField[selectedRow][selectedCol] = activePlayer + 1;
+    // console.log(gameField);
+    const winnerID = gameOver();
+    console.log(winnerID);
+
+    endGame(winnerID);
     switchPlayer();
+    currentRound++;
+
+}
+
+function gameOver(){
+    for(let i = 0; i < 3; i++){
+        //check winner by rows
+        if(gameField[i][0] > 0 && 
+            gameField[i][0] == gameField[i][1] && 
+            gameField[i][1] == gameField[i][2]
+        ){
+            // console.log(gameField[i][0]);
+            return gameField[i][0];
+        }
+        //check winner by coloumn
+        if(gameField[0][i] > 0 && 
+            gameField[0][i] == gameField[1][i] &&
+            gameField[1][i] == gameField[2][i]
+        ){
+            // console.log(gameField[0][i]);
+            return gameField[0][i];
+        }
+        
+    }
+
+    //check winner diagonally, bottom left to top right
+    if(gameField[0][2] > 0 &&
+        gameField[0][2] == gameField[1][1] &&
+        gameField[1][1] == gameField[2][0]    
+    ){
+        // console.log(gameField[0][2]);
+        return gameField[0][2];
+    }
+
+    //check winner diagonally, top left to bottom right
+    if(gameField[0][0] > 0 &&
+        gameField[0][0] == gameField[1][1] &&
+        gameField[1][1] == gameField[2][2]    
+    ){
+        // console.log(gameField[0][0]);
+        return gameField[2][2];
+    }
+
+    if(currentRound === 9){
+        return -1;
+    }
+
+    return 0;
+}
+
+function endGame(winnerID){
+
+    if(winnerID > 0){
+         endGameMessage.style.display = "block";
+         endGameMessage.firstElementChild.firstElementChild.textContent = player[winnerID-1].name;
+         winnerFound = true;
+    }
+    if(winnerID === -1){
+         endGameMessage.style.display = "block";
+         endGameMessage.firstElementChild.textContent = "It's a DRAW";
+    }
 }
